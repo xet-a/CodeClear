22d5c7cee427ba00d5e7dcf00acdfd324af4340e
ubuntu <tksop59@naver.com>
2021-07-06 04:24:34 -0700

    closet01

--- server/closet/views.py
+++ server/closet/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Closet
+
+
+def index(request):
+
+    clothes_list = Closet.objects.order_by('clothes_category')
+    context = {'clothes_list': clothes_list}
+
+
+    return render(request, 'shop/clothes_list.html', context)
